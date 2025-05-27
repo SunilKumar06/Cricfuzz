b846dd7cb796210b35bbad1f4440179d6844d2a8
Sunilkumar06 <sunilvj006@gmail.com>
2025-05-27 16:47:10 +0530

    Initial commit

---
+++ src/main/java/com/CricStats/Cricfuzz/CricfuzzApplication.java
@@ -0,0 +1,13 @@
+package com.CricStats.Cricfuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricfuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricfuzzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CricStats/Cricfuzz/CricfuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CricStats.Cricfuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricfuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
